58ecbd9b27221d982d22374a5c484c72da67086c
Lucas Douglas Reis de Oliveira <LucasDouglas-10@hotmail.com>
2018-08-30 20:13:14 -0300

    segunda parte da tela finalizando

---
+++ Doceria/View/Views/Tela1Doceria.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Doceria.Views
+{
+    /// <summary>
+    /// Interação lógica para Tela1Doceria.xam
+    /// </summary>
+    public partial class Tela1Doceria : UserControl
+    {
+        public Tela1Doceria()
+        {
+            InitializeComponent();
+        }
+
+        private void CarregarTelaAPedidos_Click(object sender, RoutedEventArgs e)
+        {
+            /* telaA minhatelaA = new telaA();
+             * minhatelaA.show();
+             * */
+        }
+
+        private void CarregarTelaCadastros_Click(object sender, RoutedEventArgs e)
+        {
+            /* telaB minhatelaB = new telaB();
+             * minhatelaB.showDialog();
+             * */
+        }
+    }
+}
